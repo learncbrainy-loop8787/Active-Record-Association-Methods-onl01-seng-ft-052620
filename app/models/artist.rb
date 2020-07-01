ce80736c811b0ac512caa5064354e7ff10eab356
korey ko <koreykendrick1@gmail.com>
2020-07-01 18:14:47 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,7 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  def get_genre_of_first_song
+  def self.get_genre_of_first_song
     self.genre.first
   end
 
